e33cb9e7ea3b2a1bc56ad659e003e923743c8a8f
Sifat2245 <mdsifat1015@gmail.com>
2025-12-01 09:10:36 +0600

    craeted a file organizer cli

--- file-organizer.js
+++ file-organizer.js
@@ -58,5 +58,81 @@ initializeDirectories();
 
 const getCategory = (fileName) => {
   const ext = path.extname(fileName).toLowerCase();
-  
+  for (const [category, extensions] of Object.entries(categories)) {
+    if (extensions.includes(ext)) {
+      return category;
+    }
+  }
+
+  return "others";
 };
+
+const organizeFiles = () => {
+  console.log("file organizer \n");
+  console.log("source:", sourceDir);
+  console.log("destination", organizedDir);
+  console.log("\n" + "-".repeat(50) + "\n");
+
+  const files = fs.readdirSync(sourceDir);
+
+  if (files.length === 0) {
+    console.log("no files to work on!");
+    return;
+  }
+  console.log(`found ${files.length} file to organize \n`);
+
+  const stats = {
+    total: 0,
+    byCategory: {},
+  };
+
+  files.forEach((file) => {
+    const sourcePath = path.join(sourceDir, file);
+    const stat = fs.statSync(sourcePath);
+    if (stat.isDirectory()) {
+      return;
+    }
+
+    const category = getCategory(file);
+    const destDir = path.join(organizedDir, category);
+    const destPath = path.join(destDir, file);
+    fs.copyFileSync(sourcePath, destPath);
+
+    stats.total++;
+    stats.byCategory[category] = (stats.byCategory[category] || 0) + 1;
+
+    console.log(`${file}`);
+    console.log(`${category}`);
+    console.log(`${stat.size}`);
+  });
+};
+
+const showHelp = () => {
+  console.log(`
+    file organizer - usage:
+
+    command: 
+    init - create files
+    organize - organize files into categories
+
+    example: 
+    node file-organizer init
+    node file-organizer organize
+
+    `);
+};
+
+const command = process.argv[2];
+
+switch (command) {
+  case "init":
+    initializeDirectories();
+    break;
+
+  case "organize":
+    organizeFiles();
+
+  default:
+    showHelp();
+    break;
+}
